4ea5088c47c7a08b0e429927f3a4d6fe1f2daada
(Max) Loo Pin Mok <maxloo@smu.edu.sg>
2022-12-11 20:53:46 +0800

    updated README

--- New/buildREADME.py
+++ New/buildREADME.py
@@ -2,9 +2,17 @@ import os
 
 path = "/mnt/c/Users/Max/MaxVault"
 write_path = "/mnt/c/Users/Max/MaxVault/README.md"
+tagCount = "/mnt/c/Users/Max/MaxVault/tagCount.txt"
 os.chdir(path)
 
 with open(write_path, 'w') as f:
+  f.write("### Tag: Count\n")
+  with open(tagCount, 'r') as g:
+    tagCount_lines = g.readlines()
+    for line in tagCount_lines:
+      f.write("- " + line)
+    g.close()
+  f.write("\n### Blog Posts\n")
   for file in os.listdir():
     if file.endswith(".md"):
       if file != "README.md":
@@ -12,5 +20,5 @@ with open(write_path, 'w') as f:
         rejoin_name = '%20'.join(split_name)
         github_link = "https://github.com/maxloosmu/MaxVault/blob/main/" + rejoin_name
         file_name = file[:-3]
-        f.write(f"[{file_name}]({github_link})\n\n")
+        f.write(f"- [{file_name}]({github_link})\n")
   f.close()
